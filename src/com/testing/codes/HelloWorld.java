77e3fce2a9c4f34f7a208d05e4945f3749600dad
KamalLalSah <kamalsahnow@gmail.com>
2019-04-12 13:58:41 +0530

    initial codes

---
+++ src/com/testing/codes/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.testing.codes;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hello World!");
+		System.out.println("new added123");
+
+	}
+
+}
---
+++ src/com/testing/codes/test.java
@@ -0,0 +1,10 @@
+package com.testing.codes;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("test123");
+
+	}
+
+}
